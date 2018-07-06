7aa23a4716354586d6ee8b1611202727f9aa9238
Mariajose <tienditacote@gmail.com>
2018-07-05 20:25:44 -0400

    responsive

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+
+
+$(document).ready(function(){
+ var elemento = $('#elemento');
+
+  
+		$('#elemento').toggle();
+	
+});
+
+
+  // $('.btn-habilidades').on('click', function(){
+	//$('#about').hide();
+
